cf8c3c4a9e4be79fcbcd781c4f87b6e487c8ccca
guy <guy.selenium@gmail.com>
2020-07-04 23:00:36 +0300

    Add mvc

---
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Models.DTO;
+using MicroRabbit.MVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransferService _transferService;
+
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            var transferDto = new TransferDto
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmount = model.TransferAmount
+            };
+
+            await _transferService.Transfer(transferDto);
+
+            return View("Index");
+        }
+    }
+}
